90c5dc5462ab8da63ae62f45e6c1cd70c176299b
ZeroOne9 <caochihuy79@gmail.com>
2024-10-31 15:16:58 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "To my Lover <3",
+    introTitle: 'Kim Tú',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
