cada1f9c72c3b63824c377a833a653ca42da3bc2
ZeroOne9 <caochihuy79@gmail.com>
2024-10-31 15:39:43 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,18 @@
 const CONFIG = {
     titleWeb: "To my Lover <3",
     introTitle: 'Kim Tú',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introDesc: `Trên trời có triệu vì sao
+    xếp thành 4 chữ vì sao yêu nàng`,
     btnIntro: '^^HiHi^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao em lại iu anh dọ',
+    btnReply: 'Gửi cho aiu <3',
     reply: 'Tại vì anh đẹp trai quá chài hihi',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi ps5 nha.',
+    mess: 'Em xạo qó ò nma anh thích 🥰. Yêu em bự 😘😘',
+    messDesc: 'Tối nay 7h, anh qua đón đi chơi ps5 nha.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    messLink: 'https://www.facebook.com/XcrossSaber00/' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
