4d2bf883ac60368036caee2a6dc9699c4cd76b71
ZeroOne9 <caochihuy79@gmail.com>
2024-11-09 23:10:26 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 const CONFIG = {
     titleWeb: "To my Lover <3",
-    introTitle: 'Kim Tú',
+    introTitle: 'Phương Phương',
     introDesc: `Trên trời có triệu vì sao
     xếp thành 4 chữ vì sao yêu nàng`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao em lại iu anh dọ',
+    title: 'I wanna say sorry. I knew, I made a mistake',
+    desc: 'I hope, you will give me a chance to change myself. I hope we can make peace with each other. however, the choice is yours  ',
+    btnYes: 'Em sẽ tha lỗi cho anh, iu anh<3',
+    btnNo: 'Không,it's over',
+    question: 'Em chọn tha lỗi cho anh ròi thì anh cũng hy vọng em cũng sẽ thay đổi để in the future we have each other and have a happy life :3',
     btnReply: 'Gửi cho aiu <3',
-    reply: 'Tại vì anh đẹp trai quá chài hihi',
-    mess: 'Em xạo qó ò nma anh thích 🥰. Yêu em bự 😘😘',
-    messDesc: 'Tối nay 7h, anh qua đón đi chơi ps5 nha.',
+    reply: 'Em cũng xin lỗi vì đã làm tổn thương anh,em sẽ hạ cái tôi xuống và lắng nghe anh nhiều hơn để hiểu anh hơn',
+    mess: 'Cảm ơn vì đã cho anh cơ hội🥰. Yêu em bự 😘😘',
+    messDesc: 'Anh qua đón em đi chơi nhe.if you are in VietNam hihi',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/XcrossSaber00/' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
