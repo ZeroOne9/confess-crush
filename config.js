b0c6db328d758d4e6422e210f43a280f145b8b30
ZeroOne9 <caochihuy79@gmail.com>
2024-11-10 04:16:48 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,19 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    titleWeb: "To my Lover <3",
+    introTitle: 'Phương Phương',
+    introDesc: `I wanna say sorry. I knew, I made a mistake
+    I hope, you will give me a chance to change myself.
+    I hope we can make peace with each other. however, the choice is yours`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    title: 'I'm sorry don't leave me  🥰',
+    desc: 'I love you so much ',
+    btnYes: 'Tha lỗi <3',
+    btnNo: 'Không nha',
+    question: 'Em chọn tha lỗi cho anh ròi thì anh cũng hy vọng em cũng sẽ thay đổi để in the future we have each other and have a happy life :3 ',
+    btnReply: 'Gửi cho aiu',
+    reply: 'Em cũng xin lỗi vì đã làm tổn thương anh,em sẽ hạ cái tôi xuống và lắng nghe anh nhiều hơn để hiểu anh hơn',
+    mess: 'mãi iu à <3',
+    messDesc: 'Thật ra anh biết em sẽ từ chối. I wish you all',
+    btnAccept: 'hihi',
+    messLink: 'https://www.facebook.com/XcrossSaber00/' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
